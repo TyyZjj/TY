ff37afe0468f9118b06001ce819b88c56b7903b2
Taoyuyu <yuyu1.tao@rsopto.net>
2025-01-23 15:20:44 +0800

    类工厂拓展

--- TCommon/Extend/DesignPattern/TFactory.h
+++ TCommon/Extend/DesignPattern/TFactory.h
@@ -14,6 +14,7 @@
 *BaseClass: 基类的类名
 */
 #define KernelFactory(FactoryClass, BaseClass)\
+template<typename KEY>\
 class FactoryClass\
 {\
 public:\
@@ -21,27 +22,27 @@ public:\
 \
 	template<typename T>\
 	struct sRegisterT{\
-		sRegisterT(const QString& key){\
+		sRegisterT(const KEY& key){\
 			FactoryClass::GetKernel()->m_mapRegistor.insert(key, [] { return new T(); });}\
 \
 		template<typename... Args>\
-		sRegisterT(const QString& key, Args... args){\
+		sRegisterT(const KEY& key, Args... args){\
 			FactoryClass::GetKernel()->m_mapRegistor.insert(key, [&] { return new T(args...); });}\
 	};\
 \
-	BaseClass* Produce(const QString& key){\
+	BaseClass* Produce(const KEY& key){\
 		if (!m_mapRegistor.contains(key)){return nullptr;}\
 		return m_mapRegistor.value(key)();}\
 \
 	QSharedPointer<BaseClass> ProduceSharePointer(const QString& key){\
 		return QSharedPointer<BaseClass>(Produce(key));}\
 \
-	QStringList GetLstRegister(){\
+	QList<KEY> GetLstRegister(){\
 		return m_mapRegistor.keys();}\
 private:\
 	FactoryClass() {};\
 	Q_DISABLE_COPY(FactoryClass)\
-	QMap<QString, std::function<BaseClass* ()>> m_mapRegistor;\
+	QMap<KEY, std::function<BaseClass* ()>> m_mapRegistor;\
 };
 
 #define REGISTER_VNAME(T) reg_msg_##T##_
@@ -52,14 +53,14 @@ private:\
 *key: 派生类名
 *...: 派生类构造函数的参数列表
 */
-#define REGISTER(FactoryClass, T, key, ...) static FactoryClass::sRegisterT<T> REGISTER_VNAME(T)(key, ##__VA_ARGS__);
+#define REGISTER(FactoryClass, T, KEY, key, ...) \
+static FactoryClass<KEY>::sRegisterT<T> REGISTER_VNAME(T)(key, ##__VA_ARGS__);
 
-/*范例
-class Base{};
-class A :public Base{};
-KernelFactory(FactoryName, Base)
-REGISTER(FactoryName, A, "A")
-*/
+//Sample:
+//class Base {};
+//class A :public Base {};
+//KernelFactory(FactoryName, Base)
+//REGISTER(FactoryName, A, QString, "A")
 #pragma endregion
 
 #endif //_TFACTORY_H_
